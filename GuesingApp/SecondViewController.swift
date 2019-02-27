deec33a377aa7f900448b02e8ec1018d84b94b98
Nalivela,Saikumar <s533628@ol10036m.nwmissouri.edu>
2019-02-26 20:48:59 -0600

    Initial Commit

---
+++ GuesingApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GuesingApp
+//
+//  Created by Nalivela,Saikumar on 2/26/19.
+//  Copyright © 2019 Nalivela,Saikumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
